27a66bb7fb3dc0fda2d5982bd86e4a4b1d09cc88
arepresas <abraham.represas@gmail.com>
2017-01-25 13:56:11 +0100

    Organize imports

--- src/main/java/com/focalserver/datatemp/rest/dao/TempObjectDAO.java
+++ src/main/java/com/focalserver/datatemp/rest/dao/TempObjectDAO.java
@@ -1,17 +1,13 @@
 package com.focalserver.datatemp.rest.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import com.focalserver.datatemp.rest.models.TempObject;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TempObjectDAO extends JpaRepository<TempObject, Long> {
 
     public List<TempObject> findByLocalDateTimeContainingOrderByDataIdDesc(String date);
-
-
 
 }
